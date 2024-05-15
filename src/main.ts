1f572fbbe4beaa77524135afc56f1b540cb0e58a
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-05-15 21:02:14 +0800

    add: cors config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   await app.listen(process.env.GATEWAY_PORT);
 }
 bootstrap().then(() => {
